fbf41e2d3813ac058f0103788e9118dd86e91c67
joelwaiman <waimanjoel@gmail.com>
2024-05-27 23:10:36 -0300

    add: styles and brain png

--- front-end/MentalHealthApp/src/components/login/Login.jsx
+++ front-end/MentalHealthApp/src/components/login/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import { Link } from 'react-router-dom'
 
+import brain from "../../assets/brain.png"
+
 import styles from './Login.module.css'
 
 export const Login = () => {
@@ -18,31 +20,37 @@ export const Login = () => {
   }
 
   return (
-    <div className={styles.card}>
-      <h1>Iniciar Sesion</h1>
-      <form className={styles.form}
-        onSubmit={logIn}>
-        <input className={styles.input}
-          placeholder="Nombre de usuario o email"
-          type="text"
-          name="user"
-          onChange={catchInputs} />
-        <input className={styles.input}
-          placeholder="Contraseña"
-          type="password"
-          name="password"
-          onChange={catchInputs} />
+    <>
+      <img className={styles.brain}
+        src={brain}
+        alt="cerebro" />
+      <div className={styles.card}>
+        <h1>Iniciar Sesion</h1>
+        <form className={styles.form}
+          onSubmit={logIn}>
+          <input className={styles.input}
+            placeholder="Usuario o correo electronico"
+            type="text"
+            name="user"
+            onChange={catchInputs} />
+          <input className={styles.input}
+            placeholder="Contraseña"
+            type="password"
+            name="password"
+            onChange={catchInputs} />
+          <div className={styles.checkbox_container}>
+            <input type="checkbox" />
+            <span>Recordarme</span>
+          </div>
+          <button className={styles.button}>
+            Ingresar
+          </button>
+        </form>
         <span>
-          <input type="checkbox" />
-          Recordarme
+          No tienes cuenta? <Link className={styles.link} to="createuser">Registrate</Link>
         </span>
-        <button className={styles.button}>
-          Login
-        </button>
-      </form>
-      <span>
-      No tienes cuenta? <Link to="createuser">Registrate</Link>
-      </span>
-    </div>
+      </div>
+    </>
+
   )
 }
