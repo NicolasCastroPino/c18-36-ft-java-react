b7261aefc693126c507dce64236e86de456d00c3
Cristian-Maxi <cristiangomez0686@outlook.com>
2024-06-07 19:15:50 -0300

    Actualizacion del Cors Configuration

--- back-end/src/main/java/com/healthtech/demo/controllers/CSRFController.java
+++ back-end/src/main/java/com/healthtech/demo/controllers/CSRFController.java
@@ -9,6 +9,7 @@ public class CSRFController {
     
     @GetMapping("/csrf-token")
     public CsrfToken getCsrfToken(CsrfToken token) {
+        System.out.println("CSRF Token Requested: " + token.getToken());
         return token;
     }
 }
--- back-end/src/main/java/com/healthtech/demo/corsConfig/CorsConfiguration.java
+++ back-end/src/main/java/com/healthtech/demo/corsConfig/CorsConfiguration.java
@@ -9,9 +9,9 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://tu-dominio-de-produccion.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "X-XSRF-TOKEN","Authorization")
+                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "X-XSRF-TOKEN", "Authorization")
                 .allowCredentials(true);
         System.out.println("CORS configuration applied successfully.");
     }
