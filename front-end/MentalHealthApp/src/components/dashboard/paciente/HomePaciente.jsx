7d763a948131d753c9c8b3689d5670d94814ffc1
Your Namemgit config --global user.email 'mariasol039@hotmail.comgit commit  git config --global user.email you@example.comgit commit -m 'Add new'git pushgit config --global user.email you@example.com  git config --global user.name Your <you@example.com>
2024-06-07 04:05:41 -0300

    Add(-): DailyEmotions and CardEmotions

--- front-end/MentalHealthApp/src/components/dashboard/paciente/HomePaciente.jsx
+++ front-end/MentalHealthApp/src/components/dashboard/paciente/HomePaciente.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
 import Sidebar from '../sidebar/Sidebar';
-//import './styles/HomePaciente.module.css';
+import './HomePaciente.css';
+import DataPaciente from './dataPaciente/DataPaciente';
+import DailyEmotions from './dailyEmotions/DailyEmotions';
 
 const HomePaciente = () => {
   return (
-    <div className="dashboard">
-      <Sidebar></Sidebar>
-      <h1>hola paciente</h1>
-
+    <div className="dashboard-paciente">
+      <Sidebar />
+      <div className="content">
+        <div className="data-paciente">
+          <DataPaciente />
+        </div>
+        <div className='daily-part'>
+          <DailyEmotions></DailyEmotions>
+        </div>
+       
+        <h1>hola paciente</h1>
+      </div>
     </div>
   );
 };
 
 export default HomePaciente;
+
+
+
+// import React from 'react';
+// import Sidebar from '../sidebar/Sidebar';
+// import './HomePaciente.css';
+// import DataPaciente from "./dataPaciente/DataPaciente"
+
+// const HomePaciente = () => {
+//   return (
+//     <div className="dashboard-paciente">
+//       <Sidebar></Sidebar>
+//       <div className='data-paciente'>
+//       <DataPaciente></DataPaciente>
+//       </div>
+//       <hr className="divider" />
+      
+//       <h1>hola paciente</h1>
+
+//     </div>
+//   );
+// };
+
+// export default HomePaciente;
---
+++ front-end/MentalHealthApp/src/components/dashboard/paciente/emotionCard/EmotionCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./EmotionCard.css"
+
+
+const EmotionCard = () =>{
+return(
+    <div className="emotionCard-container">
+        <div className="info-emotion">
+       
+        <p><strong>Fecha:</strong> 03/05/2024</p>
+        <p><strong>Emoción</strong> Tristeza</p>
+        <p><strong>Descripción:</strong> bla bla bla bla bla</p>
+        
+       
+      </div>
+
+
+    </div>
+)
+}
+
+export default EmotionCard
